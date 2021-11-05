41753fb47248c8fa0114ae7557e92f35e3c0b1c4
maher <mahermnsour@gmail.com>
2021-11-05 16:56:18 +0200

    test

---
+++ api/WebApplication1/WebApplication1/Controllers/productController.cs
@@ -0,0 +1,45 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class productController : ControllerBase
+    {
+        private readonly IConfiguration _configuration;
+
+        public productController(IConfiguration configuration)
+        {
+            _configuration = configuration;
+        }
+        [HttpGet]
+        public JsonResult Get()
+        {
+            string query = @"select name ,image,price from product";
+            DataTable table = new DataTable();
+            string sqlDataSource = _configuration.GetConnectionString("pro");
+            MySqlDataReader myReader;
+            using(MySqlConnection mycon  = new MySqlConnection(sqlDataSource))
+            {
+                mycon.Open();
+                using(MySqlCommand mycommand =new MySqlCommand(query, mycon))
+                {
+                    myReader = mycommand.ExecuteReader();
+                    table.Load(myReader);
+                    myReader.Close();
+                    mycon.Close();
+
+                }
+                return new JsonResult(table);
+            }
+        }
+    }
+}
---
+++ api/WebApplication1/WebApplication1/Model/product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace WebApplication1.Model
+{
+    public class product
+    {
+        public int ID { get; set; }
+        public string name { get; set; }
+        public string image { get; set; }
+        public int price { get; set; }
+    }
+}
